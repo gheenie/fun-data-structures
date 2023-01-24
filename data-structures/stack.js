1d14fb2d34750a80e3cde0c5750b6f7753a8d184
infoomics <sbx_94@hotmail.com>
2023-01-24 14:50:15 +0000

    built test case push()

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,5 +1,18 @@
-function createStack() {
+function createStack(maxSize = 5) {
   // build your stack object inside this factory function
+
+  const stack = {};
+  stack.quantity = 0;
+  stack.storage = {};
+  stack.maxSize = maxSize;
+  stack.push = push;
+  stack.index = 0;
+
+  return stack;
+}
+
+function push(item) {
+  this.storage[++this.index] = item;
 }
 
 module.exports = createStack;
