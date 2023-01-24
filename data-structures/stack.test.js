a8589b9480398116a00c2b6a5cfa2c08fb507339
infoomics <sbx_94@hotmail.com>
2023-01-24 15:00:44 +0000

    added max test for push

--- data-structures/stack.test.js
+++ data-structures/stack.test.js
@@ -17,12 +17,19 @@ describe('', () => {
         expect(testStack.maxSize).toBe(10);
     });
 
-    test('push method', () => {
+    test('push method; nothing happens past max items', () => {
         const testStack = createStack();
 
         testStack.push('apple');
         testStack.push('orange');
 
         expect(testStack.storage).toEqual({ 1: 'apple', 2: 'orange' });
+
+        testStack.push('c');
+        testStack.push('d');
+        testStack.push('e');
+        testStack.push('past');
+
+        expect(testStack.storage).toEqual({ 1: 'apple', 2: 'orange', 3: 'c', 4: 'd', 5: 'e' });
     });
 });
