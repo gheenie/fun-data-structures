266174cc21b2480bcaaf9e033b7f0f1dbd633f79
infoomics <sbx_94@hotmail.com>
2023-01-24 14:49:42 +0000

    added tests for push()

--- data-structures/stack.test.js
+++ data-structures/stack.test.js
@@ -1,11 +1,28 @@
 const createStack = require('./stack');
 
 describe('', () => {
-    const testStack = createStack();
+    test('default constructor', () => {
+        const testStack = createStack();
 
-    test('default values', () => {
         expect(testStack.quantity).toBe(0);
         expect(testStack.storage).toEqual({});
         expect(testStack.maxSize).toBe(5);
+    });
+
+    test('constructor with an argument', () => {
+        const testStack = createStack(10);
+
+        expect(testStack.quantity).toBe(0);
+        expect(testStack.storage).toEqual({});
+        expect(testStack.maxSize).toBe(10);
+    });
+
+    test('push method', () => {
+        const testStack = createStack();
+
+        testStack.push('apple');
+        testStack.push('orange');
+
+        expect(testStack.storage).toEqual({ 1: 'apple', 2: 'orange' });
     });
 });
