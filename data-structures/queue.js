6d9b2869b17300b586a4b51a734fb808ed89f569
infoomics <sbx_94@hotmail.com>
2023-01-25 10:35:20 +0000

    refactored to use prototype

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -1,56 +1,47 @@
+const queueProto = {
+    enQueue: function(item) {
+        if (this.back - this.front === this.maxSize - 1) throw new Error('queue is full');
+
+        this.storage[++this.back] = item;
+    },
+    deQueue: function() {
+        if (this.back - this.front === -1) throw new Error('queue is empty');
+
+        const deQueuedItem = this.storage[this.front];
+    
+        delete this.storage[this.front++];
+    
+        return deQueuedItem;
+    },
+    getQuantity: function() {
+        return this.back - this.front + 1;
+    },
+    isEmpty: function() {
+        if (this.getQuantity() !== 0) return false;
+
+        for (const i in this.storage) return false;
+    
+        return true;
+    },
+    isFull: function() {
+        if (this.getQuantity() === this.maxSize) return true;
+
+        return false;
+    },
+    peek: function() {
+        return this.storage[this.front];
+    }
+};
+
 function createQueue(maxSize) {
-    const queue = {};
+    const queue = Object.create(queueProto);
 
     queue.maxSize = maxSize;
     queue.front = 1;
     queue.back = 0;
     queue.storage = {};
-    queue.enQueue = enQueue;
-    queue.deQueue = deQueue;
-    queue.getQuantity = getQuantity;
-    queue.isEmpty = isEmpty;
-    queue.isFull = isFull;
-    queue.peek = peek;
     
     return queue;
-}
-
-function enQueue(item) {
-    if (this.back - this.front === this.maxSize - 1) throw new Error('queue is full');
-
-    this.storage[++this.back] = item;
-}
-
-function deQueue() {
-    if (this.back - this.front === -1) throw new Error('queue is empty');
-
-    const deQueuedItem = this.storage[this.front];
-
-    delete this.storage[this.front++];
-
-    return deQueuedItem;
-}
-
-function getQuantity() {
-    return this.back - this.front + 1;
-}
-
-function isEmpty() {
-    if (this.getQuantity() !== 0) return false;
-
-    for (const i in this.storage) return false;
-
-    return true;
-}
-
-function isFull() {
-    if (this.getQuantity() === this.maxSize) return true;
-
-    return false;
-}
-
-function peek() {
-    return this.storage[this.front];
 }
 
 module.exports = createQueue;
