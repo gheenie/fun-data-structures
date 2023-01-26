eeeee997af0306a9800866da7d1c8dc41e8b03f4
infoomics <sbx_94@hotmail.com>
2023-01-26 01:55:15 +0000

    rearranged getChildrenOfLastParent

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -1,37 +1,19 @@
 const treeProto = {
-    /*addData: function(nodeToAdd, parentsStr) {
-        // parentsStr is a string with whitespaces between parent nodes.
-        const parentNodes = parentsStr.split(' ');
+    getChildrenOfLastParent: function(currentObj, parentsSubstr) {
+        const  whitespaceIndex = parentsSubstr.indexOf(' ');
 
-        function addToLastParentNode(childNode, currentObj, i) {
-            if (i === parentNodes.length - 1) {
-                if ( currentObj[parentNodes[i]].hasOwnProperty(childNode) ) throw new Error('node already exists');
-
-                currentObj[parentNodes[i]][childNode] = {};
-            } else {
-                addToLastParentNode(childNode, currentObj[parentNodes[i]], ++i);
-            }
+        if (whitespaceIndex === -1) {
+            return currentObj[parentsSubstr];
+        } else {
+            currentObj = currentObj[ parentsSubstr.substring(0, whitespaceIndex) ];
+            parentsSubstr = parentsSubstr.substring(whitespaceIndex + 1);
+            return this.getChildrenOfLastParent(currentObj, parentsSubstr);
         }
-
-        addToLastParentNode(nodeToAdd, this.storage, 0);
-    }*/
-
+    },
     addData: function(node, parentsStr) {
         // parentsStr is a string with whitespaces between parent nodes.
         
-        function getChildrenOfLastParent(currentObj, parentsSubstr) {
-            const  whitespaceIndex = parentsSubstr.indexOf(' ');
-
-            if (whitespaceIndex === -1) {
-                return currentObj[parentsSubstr];
-            } else {
-                currentObj = currentObj[ parentsSubstr.substring(0, whitespaceIndex) ];
-                parentsSubstr = parentsSubstr.substring(whitespaceIndex + 1);
-                return getChildrenOfLastParent(currentObj, parentsSubstr);
-            }
-        }
-
-        const childrenOfLastParent = getChildrenOfLastParent(this.storage, parentsStr);
+        const childrenOfLastParent = this.getChildrenOfLastParent(this.storage, parentsStr);
 
         if ( childrenOfLastParent === undefined ) throw new Error('at least one parent does not exist');
 
